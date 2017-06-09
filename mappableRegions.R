4df83ca40fc2818a985214093588101887f564fb
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-06-09 13:10:43 +0300

    small progress toward understanding of the pipeline...

--- mappableRegions.R
+++ mappableRegions.R
@@ -1,18 +1,35 @@
 args<-commandArgs(trailingOnly = TRUE)
+print(args)
+
 library(parallel)
 library(foreach)
 library(doParallel)
 cl<-parallel::makeCluster(2)
 registerDoParallel(cl)
-hgall<-c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", 
-	"chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", 
-	"chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY")
-if(arg[2]=="hg")chrvec<-hgall
-else if(arg[2]=="hgdm")chrvec<-c(hgall,"dm_chrX","dm_chr2L","dm_chr2R",
-	"dm_chr3L","dm_chr3R","dm_chr4")
+hgall<-c("chr1", "chr2")
+
+# "chr3", "chr4", "chr5", "chr6", 
+#	"chr7", "chr8", "chr9", "chr10", "chr11", "chr12",
+#	"chr13", "chr14", "chr15", 
+#	"chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22",
+#	"chrX", "chrY")
+
+
+chrvec<-hgall
+
+chrom <- "chr1";
+command <- paste("python generate.reads.py ",args[2]," ",chrom,
+		"|bowtie -S -t -v 0 -m 1 -f ",args[1],
+		" - |python mappable.regions.py > data/readsim/",args[2],
+		".txt 2> readsim/",args[2],".stderr",sep="");
+print(command);
+
+
 bychrom<-foreach(chrom=chrvec) %dopar% {
-	system(paste("python generate.reads.py ",arg[1]," ",chrom,
-		"|bowtie -S -t -v 0 -m 1 -f ",arg[2],
-		" - |python mappable.regions.py > readsim/",arg[2],
-		".txt 2> readsim/",arg[2],".stderr",sep=""))
+	command <- paste("python generate.reads.py ",args[2]," ",chrom,
+			"|bowtie -S -t -v 0 -m 1 -f ",args[1],
+			" - |python mappable.regions.py > data/readsim/",args[2],
+			".txt 2> readsim/",args[2],".stderr",sep="");
+	print(command);
+	# system(command)
 }
---
+++ notes.md
@@ -0,0 +1,56 @@
+
+
+## Install tools
+
+```
+conda config --add channels bioconda
+conda instal bowtie2 samtools bcftools
+conda install wgsim
+
+```
+
+## Sam tools tutorial
+
+```
+http://biobits.org/samtools_primer.html
+```
+
+
+## Build the HG19 bowtie index
+
+## Generate reads
+
+```
+cd data/hg19
+python generate.reads.py 100 chr1 > ../readsim/chr01.reads.fa 
+```
+
+The script `generate.reads.py` uses about 10G RAM. 
+
+
+## Align reads with `bowtie`
+
+```
+cd data/hg19
+bowtie -S -t -v 0 -m 1 -f genomeindex ../readsim/chr01.reads.fa > ../readsim/chr01.reads.mapping.sam
+```
+
+## Mappable regions
+
+```
+python mappable.regions.py < data/readsim/chr01.reads.mapping.sam > data/readsim/chr01.stdout.txt 2> data/readsim/chr01.stderr.txt
+```
+
+## All mappable regions
+
+```
+
+```
+
+## Sort mappable regions
+
+```
+sort -k 1,1 -k 2,2n data/readsim/chr01.stdout.txt > mappable.regions.sorted.txt
+```
+
+
