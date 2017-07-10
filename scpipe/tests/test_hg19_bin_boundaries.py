d950a64fd010ce0ec7587bb9db14a061ca444da1
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-07-10 11:16:16 +0300

    #26 replace 'bin.start.chrompos' and 'bin.end.chrompos' to 'bin.start' and 'bin.end' in bin boundaries calculation
    Task-Url: https://github.com/seqpipe/single_cell_pipeline/issues/26

--- scpipe/tests/test_hg19_bin_boundaries.py
+++ scpipe/tests/test_hg19_bin_boundaries.py
@@ -24,9 +24,9 @@ def test_bin_boundaries_generator(hg, bin_boundaries, chromosome):
         assert mappable_bin.current_size == \
             fixture_bin['mappable.positions'], \
             (index, mappable_bin, fixture_bin)
-        assert mappable_bin.start_pos == fixture_bin['bin.start.chrompos']
+        assert mappable_bin.start_pos == fixture_bin['bin.start']
         assert mappable_bin.end_pos == \
-            fixture_bin['bin.end.chrompos'], \
+            fixture_bin['bin.end'], \
             (index, mappable_bin, fixture_bin)
 
 
@@ -36,14 +36,14 @@ def test_bin_boundaries(hg, bin_boundaries, chromosome):
     fixture_df = fixture_df.reset_index()
 
     regions_df = hg.load_mappable_regions()
-    bins_df = hg.bin_boundaries([chromosome], regions_df)
+    bins_df = hg.calc_bin_boundaries([chromosome], regions_df)
 
     df = bins_df[bins_df['bin.chrom'] == chromosome]
     df = df.reset_index()
 
     assert np.all(df.columns == fixture_df.columns)
-    assert np.all(df['bin.start.chrompos'] == fixture_df['bin.start.chrompos'])
+    assert np.all(df['bin.start'] == fixture_df['bin.start'])
     assert np.all(df['bin.start.abspos'] == fixture_df['bin.start.abspos'])
-    assert np.all(df['bin.end.chrompos'] == fixture_df['bin.end.chrompos'])
+    assert np.all(df['bin.end'] == fixture_df['bin.end'])
     assert np.all(df['bin.length'] == fixture_df['bin.length'])
     assert np.all(df['mappable.positions'] == fixture_df['mappable.positions'])
