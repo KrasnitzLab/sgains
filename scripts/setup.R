77c6870d3c091facf2b6f4e90c6bbfebedb40dfa
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-10-30 14:07:27 +0200

    #27 realease new beta with updated version of SCclust
    Task-Url: https://github.com/KrasnitzLab/sgains/issues/27

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,6 +1,6 @@
 
 
-# source("https://bioconductor.org/biocLite.R")
-# biocLite("DNAcopy")
+source("https://bioconductor.org/biocLite.R")
+biocLite("DNAcopy")
 install.packages("TBEST",repos="http://cran.us.r-project.org")
 install.packages("SCclust_0.1.6.tar.gz", repos = NULL, type="source")
