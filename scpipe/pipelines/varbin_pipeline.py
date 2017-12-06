282f2de778a06d28d9c69e2bb993095426d3f3ac
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-12-06 18:10:30 +0200

    #33 verify results or varbin subcommand
    Task-Url: https://github.com/KrasnitzLab/sgains/issues/33

--- scpipe/pipelines/varbin_pipeline.py
+++ scpipe/pipelines/varbin_pipeline.py
@@ -8,8 +8,10 @@ from hg19 import HumanGenome19
 from termcolor import colored
 import os
 import pandas as pd
+import numpy as np
 import pysam
 import multiprocessing
+import traceback
 
 
 class VarbinPipeline(object):
@@ -18,45 +20,71 @@ class VarbinPipeline(object):
         self.config = config
         self.hg = HumanGenome19(config)
 
+    def find_bin_index(self, abspos, bins):
+        index = np.searchsorted(
+            abspos, bins, side='right')
+
+        index = index - 1
+        return index
+
+    def find_bin_index_binsearch(self, bins, abspos):
+        index_up = len(bins)
+        index_down = 0
+        index_mid = int((index_up - index_down) / 2.0)
+
+        while True:
+            if abspos >= int(bins[index_mid]):
+                index_down = index_mid + 0
+                index_mid = int((index_up - index_down) / 2.0) + index_mid
+            else:
+                index_up = index_mid + 0
+                index_mid = int((index_up - index_down) / 2.0) + index_down
+
+            if index_up - index_down < 2:
+                break
+
+        return index_down
+
     def varbin(self, filename):
+        try:
+            assert os.path.exists(filename), os.path.abspath(filename)
 
-        assert os.path.exists(filename), os.path.abspath(filename)
+            infile = pysam.AlignmentFile(filename, 'rb')  # @UndefinedVariable
+            bins_df = self.hg.bins_boundaries()
+            assert bins_df is not None
 
-        infile = pysam.AlignmentFile(filename, 'rb')  # @UndefinedVariable
-        bins_df = self.hg.bins_boundaries()
-        assert bins_df is not None
+            chrom_sizes = self.hg.chrom_sizes()
+            chroms = set(self.hg.CHROMS)
 
-        chrom_sizes = self.hg.chrom_sizes()
-        chroms = set(self.hg.CHROMS)
+            count = 0
+            dups = 0
+            total_reads = 0
 
-        count = 0
-        dups = 0
-        total_reads = 0
+            prev_pos = 0
+            bin_counts = defaultdict(int)
 
-        prev_pos = 0
-        bin_counts = defaultdict(int)
+            bins = bins_df['bin.start.abspos'].values
 
-        for seg in infile:
-            total_reads += 1
-            if seg.is_unmapped:
-                continue
-            chrom = seg.reference_name
-            if chrom not in chroms:
-                continue
+            for seg in infile:
+                total_reads += 1
+                if seg.is_unmapped:
+                    continue
+                chrom = seg.reference_name
+                if chrom not in chroms:
+                    continue
 
-            abspos = chrom_sizes[chrom].abspos + seg.reference_start
-            if prev_pos == abspos:
-                dups += 1
-                continue
-            count += 1
-            index = bins_df['bin.start.abspos'].searchsorted(
-                abspos, side='right')
-            assert len(index) == 1
+                abspos = chrom_sizes[chrom].abspos + seg.reference_start
+                if prev_pos == abspos:
+                    dups += 1
+                    continue
+                count += 1
+                index = self.find_bin_index_binsearch(bins, abspos)
 
-            index = index[0] - 1
-            bin_counts[index] += 1
-            prev_pos = abspos
-        print(total_reads, dups, count)
+                bin_counts[index] += 1
+                prev_pos = abspos
+            print(total_reads, dups, count)
+        except Exception:
+            traceback.print_exc()
 
         number_of_reads_per_bin = float(count) / len(bins_df)
         result = []
