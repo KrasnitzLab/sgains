d1a1dcf827657e5b6a374832523a3ee9d960ea07
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-04-23 14:34:49 +0300

    implement calc_badbins based on centroareas #35
    Task-Url: http://github.com/seqpipe/SCclust/issues/35

--- scripts/setup.R
+++ scripts/setup.R
@@ -2,5 +2,4 @@
 
 source("https://bioconductor.org/biocLite.R")
 biocLite("DNAcopy")
-install.packages("TBEST",repos="http://cran.us.r-project.org")
-install.packages("SCclust_0.1.6.tar.gz", repos = NULL, type="source")
+install.packages("futile.logger")
